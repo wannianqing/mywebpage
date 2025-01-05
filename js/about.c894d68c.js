3e09f4374216872a60e30fd3d83384a95bc34bd7
wannianqing <15979086968@163.com>
2025-01-05 07:40:05 +0000

    Deploying to gh-pages from @ wannianqing/mywebpage@14c6390d3a2626aa88a08cd9c9bac06ba6d20896 🚀

---
+++ js/about.c894d68c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmywebpage"]=self["webpackChunkmywebpage"]||[]).push([[594],{603:function(e,n,u){u.r(n),u.d(n,{default:function(){return o}});var a=u(6768);const t={class:"about"};function r(e,n){return(0,a.uX)(),(0,a.CE)("div",t,n[0]||(n[0]=[(0,a.Lk)("h1",null,"This is an about page",-1)]))}var s=u(1241);const c={},i=(0,s.A)(c,[["render",r]]);var o=i}}]);
+//# sourceMappingURL=about.c894d68c.js.map
